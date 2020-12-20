0898d05380711b058826824e2f394e6bb51ec67a
RW Hooper <rwhooperiv@gmail.com>
2020-12-20 00:36:35 -0500

    more apigateway

--- cabbie/aws/resources/apigateway.py
+++ cabbie/aws/resources/apigateway.py
@@ -34,22 +34,26 @@ class rest_api(resource):
                 'execution': ( self.__create_api, ['name', 'model'] ),
                 'complete': False
             }
-        ]
+        ] + self.init_update_actions()
 
 
     def init_update_actions(self):
         """processes the saved resource template and returns update actions, args"""
         actions = [
             {
-                'execution': ( self.__update_code, ['name', 'code', 'publish']),
+                'execution': ( self.__create_api_resources, ['name', 'api_resources']),
                 'complete': False
             },
             {
-                'execution': ( self.__update_config, ['name', 'role', 'runtime', 'handler', 'timeout', 'memory', 'environment_variables']),
+                'execution': ( self.__create_api_methods, ['name', 'api_methods']),
                 'complete': False
             },
             {
-                'execution': ( self.__add_permissions, ['name', 'permissions']),
+                'execution': ( self.__create_api_method_response, ['name', 'api_methods']),
+                'complete': False
+            },
+            {
+                'execution': ( self.__create_api_method_integration, ['name', 'api_methods']),
                 'complete': False
             }
         ]
@@ -66,7 +70,7 @@ class rest_api(resource):
         """processes the saved resource data and returns destroy actions, args"""
         return [
             {
-                'execution': ( self.__delete_function, []),
+                'execution': ( self.__delete_api, []),
                 'complete': False
             }
         ]
@@ -157,7 +161,7 @@ class rest_api(resource):
 
 
     #### resource level CREATE functions
-    def create_api_resources(self, api_resources):
+    def __create_api_resources(self, api_resources):
         new_data = {}
 
         cors_methods = []
@@ -263,7 +267,7 @@ class rest_api(resource):
 
 
     #### method level CREATE functions
-    def create_api_method(self, resource_path, method, auth_type):
+    def __create_api_methods(self, api_methods):
         args = {
             'restApiId': self.live_data['id'],
             'resourceId': self.live_data[resource_path]['id'],
@@ -275,38 +279,39 @@ class rest_api(resource):
         #self.client.put_method()
 
     
-    def create_api_method_response(self):
-        pass
+    def __create_api_method_response(self, api_methods):
+        for method in api_methods:
+            #args = dict_select(attr, ['restApiId', 'resourceId', 'httpMethod', 'responses'])
+            for response in method['responses']:
+                self.client.put_method_response(
+                    restApiId=args['restApiId'], ## TODO: change
+                    resourceId=args['resourceId'], ## TODO: change
+                    httpMethod=args['httpMethod'], ## TODO: change
+                    statusCode=response['status_code'],
+                    responseModels=response['models']
+                )
+
     
 
-    def create_api_method_integration(self):
-        pass
-
-    
-    def __add_permissions(self, name, permissions):
-
-        for permission in permissions:
-            args = {
-                'FunctionName': name,
-                'StatementId': permission['sid'], #TODO should we automate this?  do i need to make people provide this?  i feel like ideally cabbie shoul dknow if identical positions already exist and then add or not based on that, not an SID
-                'Action': permission['action'],
-                'Principal': permission['principal'],
-                'SourceArn': permission['source_arn'],
-            }
-
-            response = self.client.add_permission(**args)
-
-        #return self.live_data
-
-        return { }
+    def __create_api_method_integration(self, api_methods):
+        # args = dict_select(attr, ['restApiId', 'resourceId', 'httpMethod', 'integration'])
+        for method in api_methods:
+            self.client.put_integration(
+                restApiId=args['restApiId'], ## TODO: change
+                resourceId=args['resourceId'], ## TODO: change
+                httpMethod=args['httpMethod'], ## TODO: change
+                integrationHttpMethod=args['httpMethod'], ## TODO: change
+                type=args['integration']['type'], ## TODO: change
+                uri=args['integration']['uri'] ## TODO: change
+            )
 
 
-    def __delete_function(self):
+    def __delete_api(self):
         # response = self.__client.delete_bucket(
         #     Bucket=self.__live_data['name']
         # )
-        response = self.client.delete_function(
-            FunctionName=self.live_data['name']
+        response = self.client.delete_rest_api(
+            restApiId=self.live_data['id']
         )
 
         # TODO: if bucketnotempty err, raise DependecyNotMetError()
